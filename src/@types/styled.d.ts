d24855eadd2214033bd335e74eb927b9705c0c27
Jônatas Bueno <jonatas.livramento@gmail.com>
2022-10-13 02:28:19 -0300

    Criando tema, tipo para as variáveis do tema, criado estilo global

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,14 @@
+import 'styled-components'
+
+import { defaultTheme } from '../styles/themes/default'
+
+// armazenando o tipo inferido em defaultTheme dentro de um tipo ThemeType
+type ThemeType = typeof defaultTheme
+
+/**
+ * a linha abaixo significa que uma nova versão de tipagem para a lib styled-components está sendo criada
+ * entretando acima importamos o próprio styled-components, com isso temos a sobrescrita apenas dos typos declarados
+ */
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType { }
+}
